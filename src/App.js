8421b4c20915011b0f449730baf4e06d335ad4cb
James Mccarthy <mccarthyjames35@yahoo.com>
2024-07-30 13:19:45 -0700

    fixed loader loading issue

--- src/App.js
+++ src/App.js
@@ -1,53 +1,10 @@
 import { useEffect, useState } from 'react';
 import React from 'react';
 import './index.css';
-
-const tempMovieData = [
-  {
-    imdbID: 'tt1375666',
-    Title: 'Inception',
-    Year: '2010',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg'
-  },
-  {
-    imdbID: 'tt0133093',
-    Title: 'The Matrix',
-    Year: '1999',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg'
-  },
-  {
-    imdbID: 'tt6751668',
-    Title: 'Parasite',
-    Year: '2019',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg'
-  }
-];
-
-const tempWatchedData = [
-  {
-    imdbID: 'tt1375666',
-    Title: 'Inception',
-    Year: '2010',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
-    runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10
-  },
-  {
-    imdbID: 'tt0088763',
-    Title: 'Back to the Future',
-    Year: '1985',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
-    runtime: 116,
-    imdbRating: 8.5,
-    userRating: 9
-  }
-];
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleLeft } from '@fortawesome/free-solid-svg-icons';
+import StarRating from './StarRating';
+import Spinner from './Spinner';
 
 const KEY = '1066f3d5';
 
@@ -55,68 +12,91 @@ const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 export default function App() {
+  const [query, setQuery] = useState('mr magoo');
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
   const [isLoading, setLoading] = useState(false);
   const [error, setError] = useState('');
-  const query = 'top gun';
+  const [selectedId, setSelectedId] = useState(null);
 
-  useEffect(function () {
-    async function fetchMovies() {
-      try {
-        setLoading(true);
-        const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
-        );
+  function handleSelectMoive(id) {
+    setSelectedId((selectedId) => (id === selectedId ? null : id));
+  }
 
-        if (!res.ok) throw new Error('Failed to fetch movies');
+  function handleCloseMovie() {
+    setSelectedId(null);
+  }
 
-        const data = await res.json();
+  useEffect(
+    function () {
+      async function fetchMovies() {
+        try {
+          setLoading(true);
+          setError('');
+          const res = await fetch(
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+          );
 
-        if (data.Response === 'False') throw new Error('Movie not found');
+          if (!res.ok) throw new Error('Failed to fetch movies');
 
-        setMovies(data.Search);
-        console.log(data);
-      } catch (err) {
-        console.error(err.message);
-        setError(err.message);
-      } finally {
-        setLoading(false);
+          const data = await res.json();
+
+          if (data.Response === 'False') throw new Error('Movie not found');
+
+          setMovies(data.Search);
+        } catch (err) {
+          console.error(err.message);
+          setError(err.message);
+        } finally {
+          setLoading(false);
+        }
       }
-    }
-    fetchMovies();
-  }, []);
+
+      if (query.length <= 2) {
+        setMovies([]);
+        setError('');
+        return;
+      }
+
+      fetchMovies();
+    },
+    [query]
+  );
 
   return (
     <>
       <NavBar>
-        <Search />
+        <Search query={query} setQuery={setQuery} />
         <NumResults movies={movies} />
       </NavBar>
       <Main>
         <Box>
           {/* {isLoading ? <LoadingSpinner /> : <MovieList movies={movies} />} */}
-          {isLoading && <LoadingSpinner />}
-          {!isLoading && !error && <MovieList movies={movies} />}
+          {isLoading && <Spinner />}
+          {!isLoading && !error && (
+            <MovieList movies={movies} onSelectMovie={handleSelectMoive} />
+          )}
           {error && <ErrorMessage message={error} />}
         </Box>
 
         <Box>
-          <WatchedSummary watched={watched} />
-          <WatchedMoivesList watched={watched} />
+          {isLoading && <Spinner />}
+          {selectedId ? (
+            <MovieDetails
+              selectedId={selectedId}
+              onCloseMovie={handleCloseMovie}
+            />
+          ) : (
+            <>
+              <WatchedSummary watched={watched} />
+              <WatchedMoivesList watched={watched} />
+            </>
+          )}
         </Box>
       </Main>
     </>
   );
 }
-export const LoadingSpinner = () => {
-  return (
-    <div className='loading-spinner'>
-      <div className='spinner'></div>
-    </div>
-  );
-};
-
 function ErrorMessage({ message }) {
   return (
     <p className='error'>
@@ -143,8 +123,7 @@ function Logo() {
   );
 }
 
-function Search() {
-  const [query, setQuery] = useState('');
+function Search({ query, setQuery }) {
   return (
     <input
       className='search'
@@ -201,19 +180,19 @@ function Box({ children }) {
 //   );
 // }
 
-function MovieList({ movies }) {
+function MovieList({ movies, onSelectMovie }) {
   return (
-    <ul className='list'>
+    <ul className='list list-movies'>
       {movies?.map((movie) => (
-        <Movie movie={movie} key={movie.imdbID} />
+        <Movie movie={movie} key={movie.imdbID} onSelectMovie={onSelectMovie} />
       ))}
     </ul>
   );
 }
 
-function Movie({ movie }) {
+function Movie({ movie, onSelectMovie }) {
   return (
-    <li>
+    <li onClick={() => onSelectMovie(movie.imdbID)}>
       <img src={movie.Poster} alt={`${movie.Title} poster`} />
       <h3>{movie.Title}</h3>
       <div>
@@ -223,6 +202,82 @@ function Movie({ movie }) {
         </p>
       </div>
     </li>
+  );
+}
+
+function MovieDetails({ selectedId, onCloseMovie }) {
+  const [movie, setMovie] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  const {
+    Title: title,
+    Year: year,
+    Rated: rated,
+    Released: released,
+    Runtime: runtime,
+    Genre: genre,
+    Director: director,
+    Writer: writer,
+    Actors: actors,
+    Plot: plot,
+    Language: language,
+    Country: country,
+    Awards: awards,
+    Poster: poster,
+    imdbRating,
+    BoxOffice: boxOffice
+  } = movie;
+
+  useEffect(() => {
+    async function getMovieDetails() {
+      setLoading(true);
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+      );
+      const data = await res.json();
+      setMovie(data);
+      setLoading(true);
+    }
+    getMovieDetails();
+  }, [selectedId]);
+
+  return (
+    <div className='details'>
+      <header>
+        <button className='btn-back' onClick={onCloseMovie}>
+          <FontAwesomeIcon icon={faCircleLeft} style={{ color: '#6741D9' }} />
+        </button>
+        <img src={poster} alt={`${title} poster`} />
+        <div className='details-overview'>
+          <h2>{title || 'No Title Available'}</h2>
+          <p>
+            {year} &bull; {rated} &bull; {released}
+          </p>
+          <p>{runtime}</p>
+          <p>{genre}</p>
+          <p>
+            <span>🌟</span>
+            {imdbRating} IMDb rating
+          </p>
+        </div>
+      </header>
+
+      <section>
+        <div className='rating'>
+          <StarRating maxRating={10} size={24} />
+        </div>
+        <p>
+          <em>{plot || 'No plot available.'}</em>
+        </p>
+        <p>Starring {actors}</p>
+        <p>Directed by {director}</p>
+        <p>Written by {writer}</p>
+        <p>Language: {language}</p>
+        <p>Country: {country}</p>
+        <p>Awards: {awards}</p>
+        <p>Box Office: {boxOffice}</p>
+      </section>
+    </div>
   );
 }
 
